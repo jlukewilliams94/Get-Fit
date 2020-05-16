a2b2d3aec0ae5f1d8fae4e5ad5f6fa0e6f99e67f
luke williams <jluke94williams@gmail.com>
2020-05-16 10:46:30 -0400

    Added html Routes

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,9 +1,20 @@
-const db = require("../models");
+const path = require("path");
 
 module.exports = function (app) {
+    //Home Screen Page Route
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/index.html"));
+    });
 
+    //New Exercise Page Route
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/exercise.html"))
+    })
 
-
+    // Statistics Page Route
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/stats.html"));
+    });
 
 
 };
